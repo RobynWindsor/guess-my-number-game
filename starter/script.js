47bfec674f1f57ad041e53ad7e33b3e1b0a5296b
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-01-27 19:10:18 +0000

    adding .textContent to restore page to original values to restart the game

--- starter/script.js
+++ starter/script.js
@@ -61,5 +61,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  const newSecretNumber = Math.trunc(Math.random() * 20) + 1;
 });
